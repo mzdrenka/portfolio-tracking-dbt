b4656b87aea7e2860c811c2ab39bad970a8954c6
Jan Snowflake <snowflakejan7@gmail.com>
2025-03-22 01:54:44 +0000

    added snapshot SNH_ABC_BANK_POSITION

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,5 +1,11 @@
+with
+    current_from_snapshot as (
+
+        select * from {{ ref("SNSH_ABC_BANK_POSITION") }} where dbt_valid_to is null
+    )
+
 select
     *,
     position_value - cost_base as unrealized_profit,
     round(unrealized_profit / cost_base, 5) as unrealized_profit_pct
-from {{ ref("STG_ABC_BANK_POSITION")}}
+from current_from_snapshot
